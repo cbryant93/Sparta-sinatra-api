862a48d8aa7fd8d9627b5c23924ef09a868b1578
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-24 13:38:27 +0000

    Set up index page

--- controllers/static_controller.rb
+++ controllers/static_controller.rb
@@ -8,8 +8,20 @@ class StaticController < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  def index
+  json = HTTParty.get("https://api.punkapi.com/v2/beers").body
+
+  @all_beers = JSON.parse(json)
+   @all_beers.each do |beers|
+   end
+  end
+
   # Landing Page
   get '/' do
     @title = "List of PUNK beers"
+
+    @all_beers = $beers
+
+    erb :'posts/index'
   end
 end
